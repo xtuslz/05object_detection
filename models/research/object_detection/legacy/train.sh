f109d1b2bfedae36968da5c634cd42a1ccdd1eb9
xtuslz <xtuslz@163.com>
2019-08-26 22:10:44 +0800

    first commit

---
+++ models/research/object_detection/legacy/train.sh
@@ -0,0 +1,3 @@
+python train.py \
+--train_dir voc/train_dir/ \
+--pipeline_config_path voc/voc.config
---
+++ models/research/object_detection/train.sh
@@ -0,0 +1,4 @@
+python model_main.py \
+--pipeline_config_path voc/voc.config \
+--model_dir voc/train_dir/ \
+--num_train_steps 50000 
